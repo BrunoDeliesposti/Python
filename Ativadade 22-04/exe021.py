24bb8011c2e3d117aeb42bbbb68db9f56b6aa26f
Bruno Deliesposti <53996366+BrunoDeliesposti@users.noreply.github.com>
2020-04-22 18:51:25 -0300

    Add files via upload

---
+++ Ativadade 22-04/exe021.py
@@ -0,0 +1,6 @@
+from pygame import mixer
+mixer.init()
+mixer.music.load('exe021.mp3')
+mixer.music.play()
+input('Programa que faz você escutar qualquer \n'
+      'música a partir da biblioteca pygame')
